3f274869a542169baa794f85844d14f4b5565673
Sofonyas-Demssew <sofidemssew@gmail.com>
2022-03-10 12:06:42 -0800

    UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,6 @@
 /*
- * * File: 101-quote.c
- * * Auth: Brennan D Baraban
+ * File: 101-quote.c
+ * Auth: Brennan D Baraban
  */
 
 #include <unistd.h>
@@ -14,8 +14,8 @@
 int main(void)
 {
 		write(2,
-					      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-					      	      59);
+			"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+			 59);
 
-			return (1);
+return (1);
 }
